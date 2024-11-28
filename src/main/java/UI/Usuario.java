1850d7dead83e2e270815797b1177149bfb5b20a
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-28 18:36:20 -0500

    Update Usuario.java

--- src/main/java/UI/Usuario.java
+++ src/main/java/UI/Usuario.java
@@ -1,4 +1,44 @@
 package UI;
+import java.awt.*;
+import java.util.List;
+import javax.swing.*;
 
-public class Usuario {
+
+public class Usuario extends JFrame {
+    String nombre,email;
+    List<Playlist> PlaylistFavorita;
+
+
+    public Usuario() {
+        setTitle("Usuario");
+        setSize(500, 500);
+        setLayout(new BorderLayout());
+
+        JPanel panelIzquierdo = new JPanel();
+        JPanel panelDerecho = new JPanel();
+        JPanel panelSuperior = new JPanel();
+        JPanel panelInferior = new JPanel();
+
+        add(panelIzquierdo, BorderLayout.WEST);
+        add(panelDerecho, BorderLayout.EAST);
+        add(panelSuperior, BorderLayout.NORTH);
+        add(panelInferior, BorderLayout.SOUTH);
+    }
+
+
+    public void agregarPlaylist(){
+        agregarPlaylist();
+
+    }
+    public void eliminarPlaylist(){
+        eliminarPlaylist();
+    }
+
+
+
+
+
+
+
+
 }
