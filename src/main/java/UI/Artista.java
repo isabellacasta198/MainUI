a0c0d090e0719c6ae91df88e41e3a6e1b0835cb6
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-28 18:37:55 -0500

    Update Artista.java

--- src/main/java/UI/Artista.java
+++ src/main/java/UI/Artista.java
@@ -1,4 +1,18 @@
 package UI;
+import java.awt.*;
+import javax.swing.*;
+import java.util.List;
 
-public class Artista {
+public class Artista extends JFrame {
+    private String nombre,pais,genero;
+    List<Canciones> canciones;
+
+    public Artista(String nombre, String pais, String genero) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.genero = genero;
+    }
+    public void agregarCancion(Canciones cancion) {
+        canciones.add(cancion);
+    }
 }
