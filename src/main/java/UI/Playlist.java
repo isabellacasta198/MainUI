9b490734ea6c062d4e56b2a77b8ac775c0fbb20e
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-28 18:38:56 -0500

    Update Playlist.java

--- src/main/java/UI/Playlist.java
+++ src/main/java/UI/Playlist.java
@@ -1,4 +1,25 @@
 package UI;
+import java.awt.*;
+import javax.swing.*;
 
-public class Playlist {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Playlist extends JFrame {
+    String Nombre;
+    String descripcion;
+    List<Canciones> canciones;
+    public Playlist(String nombre , String descripcion) {
+        this.Nombre = nombre;
+        this.descripcion = descripcion;
+
+    }
+    public ArrayList<Canciones> getCanciones() {
+        ArrayList<Canciones> canciones = new ArrayList<>();
+        return canciones;
+    }
+    public abstract void agregarCanciones(Canciones cancion);
+    public abstract void eliminarCanciones(Canciones cancion);
+
 }
