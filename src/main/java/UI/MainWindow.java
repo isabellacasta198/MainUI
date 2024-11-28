7085dae6675528f453928ee2885f12984a8382fb
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-28 18:37:25 -0500

    Update MainWindow.java

--- src/main/java/UI/MainWindow.java
+++ src/main/java/UI/MainWindow.java
@@ -1,4 +1,28 @@
 package UI;
 
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setSize(800, 600);
+        setTitle("gestor de colecciones de música");
+
+        Usuario usuario = new Usuario();
+        JPanel panelIzquierdo = new JPanel();
+        JPanel panelDerecho = new JPanel();
+        JPanel panelSuperior = new JPanel();
+        JPanel panelInferior = new JPanel();
+
+        add(panelIzquierdo, BorderLayout.WEST);
+        add(panelDerecho, BorderLayout.EAST);
+        add(panelSuperior, BorderLayout.NORTH);
+        add(panelInferior, BorderLayout.SOUTH);
+
+        }
+        public static void main(String [] args){
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.setVisible(true);
+
+        }
 }
