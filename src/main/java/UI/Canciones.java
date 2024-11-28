4db87bfad9988657133d5f3e19ea62b75afee884
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-28 18:39:30 -0500

    Update Canciones.java

--- src/main/java/UI/Canciones.java
+++ src/main/java/UI/Canciones.java
@@ -1,4 +1,21 @@
 package UI;
+import java.awt.*;
+import javax.swing.*;
 
-public class Canciones {
+public class Canciones extends JFrame {
+
+    String titulo,genero,Artista;
+    int duracion;
+
+    public Canciones(String titulo, String genero, String Artista, int duracion) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.Artista = Artista;
+        this.duracion = duracion;
+
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
 }
