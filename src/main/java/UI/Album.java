dc26dbdf53fd81551cb84d9eea986edcb07a433f
isabellacasta198 <isabella.castano03@usc.edu.co>
2024-11-29 16:15:32 -0500

    Update Album.java

--- src/main/java/UI/Album.java
+++ src/main/java/UI/Album.java
@@ -7,5 +7,10 @@ public class Album extends JFrame {
     List<Playlist> playlists;
     List<Artista> artistas;
     List<Canciones> canciones;
+    public Album() {
+        this.playlists= playlists;
+        this.artistas= artistas;
+        this.canciones= canciones;
+    }
 
 }
